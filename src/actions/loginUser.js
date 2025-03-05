16245f67b9f42d283e622a3464e3de50b4b77835
Sanchit <32457437+sanchit0496@users.noreply.github.com>
2025-03-05 12:44:44 +0530

    adding

--- src/actions/loginUser.js
+++ src/actions/loginUser.js
@@ -6,28 +6,28 @@ const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
 export async function loginUser(email, password) {
-    console.log("data", email, password);
+  console.log("data", email, password);
 
-    const users = await prisma.user.findMany();
-    console.log(users);
+  const users = await prisma.user.findMany();
+  console.log(users);
 
-    let userFound = false;
-    users.forEach((user) => {
-        if (user.password === password && user.email === email) {
-            userFound = true;
-        }
-    });
-
-    if (userFound) {
-        // ✅ No await needed, directly set cookie
-        cookies().set({
-            name: "userSession",
-            value: JSON.stringify({ email: email }),
-            httpOnly: true,
-            path: "/",
-            maxAge: 60 * 60 * 24, // 1 day
-        });
+  let userFound = false;
+  users.forEach((user) => {
+    if (user.password === password && user.email === email) {
+      userFound = true;
     }
+  });
 
-    return userFound;
+  if (userFound) {
+    // ✅ No await needed, directly set cookie
+    cookies().set({
+      name: "userSession",
+      value: JSON.stringify({ email: email }),
+      httpOnly: true,
+      path: "/",
+      maxAge: 60 * 60 * 24, // 1 day
+    });
+  }
+
+  return userFound;
 }
